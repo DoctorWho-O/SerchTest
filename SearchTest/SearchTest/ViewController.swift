6e131a01faed19cf5e273145a5b1242aa1ecfeff
DW <dw@DWdeMacBook-Pro.local>
2020-08-15 18:52:41 +0800

    添加假数据

--- SearchTest/SearchTest/ViewController.swift
+++ SearchTest/SearchTest/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var searchResults = [String]()
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
@@ -20,6 +20,35 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
 }
 
+extension ViewController:UISearchBarDelegate{
+    
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchResults = []
+        for i in 0...2 {
+            searchResults.append(String(format: "Fake Result %d for '%@'", i,searchBar.text!))
+        }
+        tableView.reloadData()
+    }
+    
+}
+
+extension ViewController:UITableViewDelegate,UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResults.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "SearchResultCell"
+        var cell:UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
+        }
+        cell.textLabel!.text = searchResults[indexPath.row]
+        return cell
+    }
+    
+}
