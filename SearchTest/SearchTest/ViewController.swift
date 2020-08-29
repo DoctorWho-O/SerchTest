74f49230419758e8b0c97d79d015446611126304
DW <dw@DWdeMacBook-Pro.local>
2020-08-29 12:50:16 +0800

    改变注释

--- SearchTest/SearchTest/ViewController.swift
+++ SearchTest/SearchTest/ViewController.swift
@@ -15,9 +15,10 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //如果要适配iphoneX的话也一样，因为tableView本身距离上面的就是安全距离。它空出来的距离就是给SearchBar的距离
+        //上面适配到safeArea之下，56刚好是searchBar的高度。
         tableView.contentInset = UIEdgeInsets(top: 56, left: 0, bottom: 0, right: 0)
         // Do any additional setup after loading the view.
+        
     }
 
 }
